a34c1f1a31f7298a6ac6b985a3637fa81cb6e72d
vigotseng <vigotseng@gmail.com>
2019-01-30 15:15:01 +0800

    first

---
+++ createdb.sh
@@ -0,0 +1,14 @@
+#! /bin/bash 
+# -*- encoding: utf-8 -*-
+#
+# =============================================
+#
+#      Author   : Andy Scout
+#    E-mail     : andyhuzhill@gmail.com 
+#
+#  Description  :
+#  Revision     :
+#
+# =============================================
+
+sqlite3 data.db < schema.sql
---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE player_info(
+    id   int PRIMARY KEY NOT NULL,
+    name char(255),
+    card_num char(20),
+    item char(255),
+    clothes_size char(255),
+    play_number char(255),
+    phone_number char(30)
+);
+
+COMMIT;
